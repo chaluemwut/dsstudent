d5801d7af28861de1268fca20691fe81199cfb47
chaluemwut <chaluemwut@hotmail.com>
2018-02-01 21:57:29 +0700

    Init project

---
+++ src/main/java/rmuti/ds/App.java
@@ -0,0 +1,27 @@
+package rmuti.ds;
+
+import rmuti.ds.DrawTree;
+import rmuti.ds.Node;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	
+    public static void main( String[] args ){
+		Node d = new Node("d", null, null);
+		Node e = new Node("e", null, null);
+		
+		Node f = new Node("f", null, null);
+		Node g = new Node("g", null, null);
+		
+		Node b = new Node("b", d, e);
+		Node c = new Node("c", f, g);
+		
+		Node root = new Node("a", b, c);
+				
+    		DrawTree drawTree = new DrawTree(root);
+    		drawTree.drawTree();
+    }
+}
---
+++ src/main/java/rmuti/ds/Node.java
@@ -0,0 +1,17 @@
+package rmuti.ds;
+
+public class Node {
+	private Object element;
+	Node left, right;
+	
+	public Node(Object e, Node l, Node r) {
+		element = e;
+		left = l;
+		right = r;
+	}
+	
+	@Override
+	public String toString() {
+		return element.toString();
+	}
+}
