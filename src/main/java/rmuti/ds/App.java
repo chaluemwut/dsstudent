cea37da03d86abea970edfd397fe35c2a1676fc8
chaluemwut <chaluemwut@users.noreply.github.com>
2023-02-15 09:56:36 +0700

    Update App.java
    update app

--- src/main/java/rmuti/ds/App.java
+++ src/main/java/rmuti/ds/App.java
@@ -1,8 +1,5 @@
 package rmuti.ds;
 
-import java.util.ArrayList;
-
-import cern.colt.Arrays;
 
 /**
  * Hello world!
@@ -11,33 +8,6 @@ import cern.colt.Arrays;
 public class App {
 
 	public static void main(String[] args) {
-		Node i = new Node("i", null, null);
-		Node h = new Node("h", null, null);
-		
-		Node f = new Node("f", null, null);
-		Node g = new Node("g", null, null);
-
-		Node b = new Node("b", h, i);
-		Node c = new Node("c", f, g);
-
-		Node root = new Node("a", b, c);
-		
-		final ArrayList lstNode = new ArrayList();
-		Visitor v = new Visitor() {
-
-			@Override
-			public void visit(Object e) {
-				lstNode.add(e);
-			}
-		};
-		
-		TreeTraversal traversal = new TreeTraversal();
-		traversal.preOrder(root, v);
-		System.out.println(lstNode);
-		
-		
-//		TreeDraw drawTree = new TreeDraw(root);
-//		drawTree.binaryTreeDraw();
 		
 	}
 }
